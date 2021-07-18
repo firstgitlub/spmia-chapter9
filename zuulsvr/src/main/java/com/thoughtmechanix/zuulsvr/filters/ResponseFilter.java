e70fc17de20d93814a9e6dd8f5605f57bb8a898d
zhengshuaibing <zhengshuaibing@huohua.cn>
2021-07-18 10:54:57 +0800

    源码阅读

--- zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/ResponseFilter.java
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/filters/ResponseFilter.java
@@ -9,13 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Component;
 
+/**
+ * 后置过滤器，是等请求得到响应后 到达网关的时候 做的一些拦截处理
+ */
 @Component
 public class ResponseFilter extends ZuulFilter{
     private static final int  FILTER_ORDER=1;
     private static final boolean  SHOULD_FILTER=true;
     private static final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
-
+    /**
+     * Trace 类是访问跟踪ID 和 跨度ID信息的入口点
+     */
     @Autowired
     Tracer tracer;
 
@@ -36,6 +41,9 @@ public class ResponseFilter extends ZuulFilter{
 
     @Override
     public Object run() {
+        /**
+         * 添加新HTTP响应首部 tmx-correlation-id，它包含spring-cloud-sleuth的跟踪ID
+         */
         RequestContext ctx = RequestContext.getCurrentContext();
         ctx.getResponse().addHeader("tmx-correlation-id", tracer.getCurrentSpan().traceIdString());
 
